7e0fb98a6daa1378e687e1ee570704c45434e8cb
reVeda <panky2sharma@gmail.com>
2019-03-11 00:09:46 +0530

    Add all my files

---
+++ FirstFloor.ModernUI.App/Pages/Dashboard.xaml.cs
@@ -0,0 +1,78 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstFloor.ModernUI.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : UserControl
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+        private void btnCashWithdrawal_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new ModernDialog
+            {
+                Title = "Common dialog",
+                Content = "Please Implement Face Recognition Methods."
+            };
+            dlg.Buttons = new Button[] { dlg.OkButton, dlg.CancelButton };
+            dlg.ShowDialog();
+            //this.dialogResult.Text = dlg.DialogResult.HasValue ? dlg.DialogResult.ToString() : "<null>";
+
+
+            /// <summary>
+            /// Implement OTP & Face Recognition Methods
+            /// </summary>
+
+
+
+        }
+
+        private void CashWithdrawal() { }
+
+        //MAIN METHOD TO USE CURRENT USER SELFIE AGAINST SAVED DB IMAGES
+        private bool GetFaceRecognised()
+        {
+            bool isFaceRecognised = false;
+            return isFaceRecognised;
+        }
+
+        //Get Image from Laptop Camera
+        private string GetCurrUserImage()
+        {
+            string imagePath = "";
+            return imagePath;
+        }
+
+        private bool SendOTP()
+        {
+            bool isSent = false;
+            return isSent;
+        }
+
+        private bool VerifyOTP()
+        {
+            bool isOtpVerified = false;
+            return isOtpVerified;
+        }
+
+
+    }
+}
---
+++ FirstFloor.ModernUI.App/Pages/Login.xaml.cs
@@ -0,0 +1,61 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstFloor.ModernUI.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnCardSwipe_Click(object sender, RoutedEventArgs e)
+        {
+            txtCard.Text = "6011000400000000";
+            _FormPinLogin.Visibility = Visibility.Visible;
+
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtCard.Text.Trim().Length > 0 && txtPinCode.Password == "7969")
+            {
+
+                // MOVE USER TO NEXT MAIN DASHBOARD PAGE
+                //NavigationService nav = NavigationService.GetNavigationService(this);
+                //nav.Navigate(new Uri("Dashboard.xaml", UriKind.RelativeOrAbsolute));
+                ///nav.Navigate(new Dashboard());
+                var dashboard = new Dashboard();
+                //MainWindow.ContentSourceProperty. = "dashboard"; 
+                this.Content = new Dashboard();
+            }
+            else
+            {
+                //SHOW SOME ERROR
+                var dlg = new ModernDialog
+                {
+                    Title = "Error",
+                    Content = "Please enter correct pin code."
+                };
+                dlg.Buttons = new Button[] { dlg.OkButton };
+                dlg.ShowDialog();
+            }
+        }
+    }
+}
